3e1cff62f57009fa6a407703db7ed3d6bce935cd
kevinrisqi <kevinrisqi22@gmail.com>
2022-07-28 18:31:49 +0700

    Create Sign In Page

--- lib/pages/sign_in_page.dart
+++ lib/pages/sign_in_page.dart
@@ -1,15 +1,212 @@
-// ignore_for_file: prefer_const_constructors
-
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 import 'package:flutter/material.dart';
+import 'package:flutter_shoes_store/theme.dart';
 
 class SignInPage extends StatelessWidget {
   const SignInPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text('Sign In Page'),
+    Widget header() {
+      return Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Login',
+            style: primaryTextStyle.copyWith(
+              fontSize: 24,
+              fontWeight: semiBold,
+            ),
+          ),
+          Text(
+            'Sign In to Continue',
+            style: subtitleTextStyle.copyWith(
+              fontSize: 14,
+              fontWeight: regular,
+            ),
+          ),
+        ],
+      );
+    }
+
+    Widget emailInput() {
+      return Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Email Address',
+            style: primaryTextStyle.copyWith(
+              fontSize: 16,
+              fontWeight: medium,
+            ),
+          ),
+          SizedBox(
+            height: 12,
+          ),
+          Container(
+            height: 50,
+            padding: EdgeInsets.symmetric(
+              horizontal: 16,
+            ),
+            decoration: BoxDecoration(
+              color: bgColor2,
+              borderRadius: BorderRadius.circular(12),
+            ),
+            child: Center(
+              child: Row(
+                children: [
+                  Image.asset(
+                    'assets/icon_email.png',
+                    width: 17,
+                  ),
+                  SizedBox(
+                    width: 16,
+                  ),
+                  Expanded(
+                    child: TextFormField(
+                      style: primaryTextStyle,
+                      decoration: InputDecoration.collapsed(
+                        hintText: 'Your Email Address',
+                        hintStyle: subtitleTextStyle,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          )
+        ],
+      );
+    }
+
+    Widget passwordInput() {
+      return Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Password',
+            style: primaryTextStyle.copyWith(
+              fontSize: 16,
+              fontWeight: medium,
+            ),
+          ),
+          SizedBox(
+            height: 12,
+          ),
+          Container(
+            height: 50,
+            padding: EdgeInsets.symmetric(
+              horizontal: 16,
+            ),
+            decoration: BoxDecoration(
+              color: bgColor2,
+              borderRadius: BorderRadius.circular(12),
+            ),
+            child: Center(
+              child: Row(
+                children: [
+                  Image.asset(
+                    'assets/icon_password.png',
+                    width: 17,
+                  ),
+                  SizedBox(
+                    width: 16,
+                  ),
+                  Expanded(
+                    child: TextFormField(
+                      style: primaryTextStyle,
+                      obscureText: true,
+                      decoration: InputDecoration.collapsed(
+                        hintText: 'Your Password',
+                        hintStyle: subtitleTextStyle,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          )
+        ],
+      );
+    }
+
+    Widget submitButton() {
+      return Container(
+        width: double.infinity,
+        height: 50,
+        child: ElevatedButton(
+          onPressed: () {},
+          style: ElevatedButton.styleFrom(
+            primary: primaryColor,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(12),
+            ),
+          ),
+          child: Text(
+            'Sign In',
+            style: primaryTextStyle.copyWith(
+              fontSize: 16,
+              fontWeight: medium,
+            ),
+          ),
+        ),
+      );
+    }
+
+    Widget footer() {
+      return Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            "Don't have an account?",
+            style: subtitleTextStyle.copyWith(
+              fontSize: 12,
+            ),
+          ),
+          SizedBox(
+            width: 4,
+          ),
+          Text(
+            'Sign up',
+            style: purpleTextStyle.copyWith(
+              fontSize: 12,
+              fontWeight: medium,
+            ),
+          ),
+        ],
+      );
+    }
+
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: bgColor1,
+        resizeToAvoidBottomInset: false,
+        body: Container(
+          margin: EdgeInsets.symmetric(
+            horizontal: defaultMargin,
+            vertical: defaultMargin,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              header(),
+              SizedBox(
+                height: 70,
+              ),
+              emailInput(),
+              SizedBox(
+                height: 20,
+              ),
+              passwordInput(),
+              SizedBox(
+                height: 30,
+              ),
+              submitButton(),
+              Spacer(),
+              footer(),
+            ],
+          ),
+        ),
       ),
     );
   }
--- lib/theme.dart
+++ lib/theme.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+double defaultMargin = 30.0;
+
 Color primaryColor = const Color(0xff6C5ECF);
 Color secondaryColor = const Color(0xff38ABBE);
 Color alertColor = const Color(0xffED6363);
@@ -11,11 +13,14 @@ Color bgColor2 = const Color(0xff2B2937);
 Color bgColor3 = const Color(0xff242231);
 
 Color primaryTextColor = const Color(0xffF1F0F2);
+Color subtitleTextColor = const Color(0xff504F5E);
 Color secondaryTextColor = const Color(0xff999999);
 
 TextStyle primaryTextStyle = GoogleFonts.poppins(color: primaryTextColor);
+TextStyle subtitleTextStyle = GoogleFonts.poppins(color: subtitleTextColor);
 TextStyle secondaryTextStyle = GoogleFonts.poppins(color: secondaryColor);
 TextStyle priceTextStyle = GoogleFonts.poppins(color: priceColor);
+TextStyle purpleTextStyle = GoogleFonts.poppins(color: primaryColor);
 
 FontWeight light = FontWeight.w300;
 FontWeight regular = FontWeight.w400;
