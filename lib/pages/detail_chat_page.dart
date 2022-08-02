49dba5c0c06280658ba9bf00c9a6cb52eae72e09
kevinrisqi <kevinrisqi22@gmail.com>
2022-08-02 17:47:00 +0700

    Create preview product in detail chat page

--- lib/pages/detail_chat_page.dart
+++ lib/pages/detail_chat_page.dart
@@ -8,6 +8,104 @@ class DetailChatPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Widget previewProduct() {
+      return Container(
+        width: 225,
+        height: 75,
+        margin: EdgeInsets.only(bottom: 20),
+        padding: EdgeInsetsDirectional.all(10),
+        decoration: BoxDecoration(
+          color: bgColor5,
+          borderRadius: BorderRadius.circular(12),
+          border: Border.all(
+            color: primaryColor,
+          ),
+        ),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(12),
+              child: Image.asset(
+                'assets/image_shoes_2.png',
+                width: 55,
+              ),
+            ),
+            SizedBox(
+              width: 10,
+            ),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'COURT VISION 2.0',
+                    style: primaryTextStyle,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  Text(
+                    '\$57,99',
+                    style: priceTextStyle.copyWith(
+                      fontWeight: medium,
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Image.asset(
+              'assets/button_close.png',
+              width: 22,
+            ),
+          ],
+        ),
+      );
+    }
+
+    Widget chatInput() {
+      return Container(
+        margin: EdgeInsets.all(20),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            previewProduct(),
+            Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    padding: EdgeInsets.symmetric(
+                      vertical: 12,
+                      horizontal: 16,
+                    ),
+                    height: 50,
+                    decoration: BoxDecoration(
+                      color: bgColor4,
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: TextFormField(
+                      style: primaryTextStyle,
+                      decoration: InputDecoration.collapsed(
+                        hintText: 'Type Message...',
+                        hintStyle: subtitleTextStyle,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 20,
+                ),
+                Image.asset(
+                  'assets/button_send.png',
+                  width: 50,
+                ),
+              ],
+            ),
+          ],
+        ),
+      );
+    }
+
     return Scaffold(
       backgroundColor: bgColor3,
       appBar: PreferredSize(
@@ -50,7 +148,7 @@ class DetailChatPage extends StatelessWidget {
           ),
         ),
       ),
-      bottomNavigationBar: actionBottom(),
+      bottomNavigationBar: chatInput(),
     );
   }
 }
